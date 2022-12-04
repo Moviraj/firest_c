e747c88ea3e95e61a2588e174d17bcd5c1f35df2
virajmo <mohiteviraj2@gmail.com>
2022-12-04 17:09:44 +0000

    first messege

---
+++ cal.py
@@ -0,0 +1,19 @@
+a = int(input("enter a number\n"))
+
+b = int(input("enter another number\n"))
+
+print("1.Addittion\n 2.Substraction\n 3.Multiplication\n 4.Division\n")
+
+ch = input("enter a choice\n")
+
+def addition(num1 , num2):
+
+    c =(a+b)
+
+    print(c)
+
+
+
+if ch == 1:
+
+    addition(a,b)
